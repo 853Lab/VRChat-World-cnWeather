99a70f26a9f77cc129c8e83ac25f6b2bd3732833
Sonic853 <Sonic853@qq.com>
2024-12-24 16:49:17 +0800

    移动方法

---
+++ Method.ts
@@ -0,0 +1,21 @@
+import { SignJWT, importPKCS8 } from 'https://deno.land/x/jose@v5.9.6/index.ts'
+
+export const generateToken = async (YourPrivateKey: string, YourKeyID: string, YourProjectID: string) => {
+  const privateKey = await importPKCS8(YourPrivateKey, 'EdDSA')
+  const customHeader = {
+    alg: 'EdDSA',
+    kid: YourKeyID
+  }
+  const iat = Math.floor(Date.now() / 1000) - 30
+  const exp = iat + 900
+  const customPayload = {
+    sub: YourProjectID,
+    iat: iat,
+    exp: exp
+  }
+  const token = await new SignJWT(customPayload)
+    .setProtectedHeader(customHeader)
+    .sign(privateKey)
+
+  return token
+}
--- create.ts
+++ create.ts
@@ -1,4 +1,4 @@
-import { SignJWT, importPKCS8 } from 'https://deno.land/x/jose@v5.9.6/index.ts'
+
 import { parseArgs } from "jsr:@std/cli/parse-args"
 
 const flags = parseArgs(Deno.args, {
@@ -11,24 +11,4 @@ const flags = parseArgs(Deno.args, {
 if (!flags.private || !flags.key || !flags.project) {
   console.error("Missing required flags")
   Deno.exit(1)
-}
-
-const generateToken = async (YourPrivateKey: string, YourKeyID: string, YourProjectID: string) => {
-  const privateKey = await importPKCS8(YourPrivateKey, 'EdDSA')
-  const customHeader = {
-    alg: 'EdDSA',
-    kid: YourKeyID
-  }
-  const iat = Math.floor(Date.now() / 1000) - 30
-  const exp = iat + 900
-  const customPayload = {
-    sub: YourProjectID,
-    iat: iat,
-    exp: exp
-  }
-  const token = await new SignJWT(customPayload)
-    .setProtectedHeader(customHeader)
-    .sign(privateKey)
-
-  return token
 }
